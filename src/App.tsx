e1c6469968c106dba0afaa939bcb6859fe1dcc2c
dachendev <13964975+dachendev@users.noreply.github.com>
2024-11-10 16:03:13 -0500

    add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import { useState } from "react";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const educations = ["Full Stack Open", "Purdue University"];
 
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "Python",
+  "React",
+  "Node.js",
+  "Express",
+  "PostgreSQL",
+  "MongoDB",
+  "Figma",
+];
+
+const App: React.FC = () => {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Jacob Dachenhaus</h1>
+      <p>Hi, I'm Jacob. I'm a Full Stack Developer.</p>
+      <div>
+        <h2>Socials</h2>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/dachendev/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/dachendev">GitHub</a>
+          </li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>Educations</h2>
+        <ul>
+          {educations.map((name, i) => (
+            <li key={i}>{name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Projects</h2>
+        <ul>
+          <li>Portfolio</li>
+          <li>Workout Buddy</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Skills</h2>
+        <ul>
+          {skills.map((name, i) => (
+            <li key={i}>{name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
-}
+};
 
 export default App;
