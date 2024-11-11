def425fad40c5e1a63d21fde553cccd77a37ad88
dachendev <13964975+dachendev@users.noreply.github.com>
2024-11-11 12:08:41 -0500

    add cta buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import React from "react";
 
 const socials = [
@@ -49,6 +50,10 @@ const Hero: React.FC = () => {
               <ListItemText>dachendev@gmail.com</ListItemText>
             </ListItem>
           </List>
+          <Grid container spacing={2}>
+            <Button variant="contained">Hire Me</Button>
+            <Button variant="outlined">Previous Work</Button>
+          </Grid>
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }} sx={{ textAlign: "center" }}>
           <img src="duck.jpeg" width="250" />
