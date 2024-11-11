d12fbc8acc8a1bd1caf10c7045e9d2bc1a508d89
dachendev <13964975+dachendev@users.noreply.github.com>
2024-11-11 12:04:18 -0500

    make hero have 100 height

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
-import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import WorkExperience from "./components/WorkExperience";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App: React.FC = () => {
   return (
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
+import Box from "@mui/material/Box";
 import React from "react";
 
 const socials = [
@@ -24,7 +25,7 @@ const socials = [
 
 const Hero: React.FC = () => {
   return (
-    <div>
+    <Box sx={{ height: "100vh" }}>
       <Grid container spacing={2} sx={{ paddingTop: 5 }}>
         <Grid size={{ xs: 12, sm: 6 }}>
           <h1>Jacob Dachenhaus</h1>
@@ -53,7 +54,7 @@ const Hero: React.FC = () => {
           <img src="duck.jpeg" width="250" />
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 };
 
