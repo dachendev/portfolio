9210ba92328f5e534d154418c6b648df3f3716a8
dachendev <13964975+dachendev@users.noreply.github.com>
2024-11-10 19:33:03 -0500

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import Grid from "@mui/material/Grid2";
 import Container from "@mui/material/Container";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import EmailIcon from "@mui/icons-material/Email";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
 import Education from "./components/Education";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import WorkExperience from "./components/WorkExperience";
-import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import WorkExperience from "./components/WorkExperience";
 
 const App: React.FC = () => {
   return (
